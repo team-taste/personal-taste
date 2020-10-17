1ce71adc91d06191bada3e7cbe6bc23d6bf2df03
sechun <sechun0215@gmail.com>
2020-10-17 17:32:46 +0900

    first commit

---
+++ src/main/kotlin/personaltaste/PersonalTasteApplication.kt
@@ -0,0 +1,11 @@
+package personaltaste
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PersonalTasteApplication
+
+fun main(args: Array<String>) {
+	runApplication<PersonalTasteApplication>(*args)
+}
---
+++ src/test/kotlin/personaltaste/PersonalTasteApplicationTests.kt
@@ -0,0 +1,16 @@
+package personaltaste
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class PersonalTasteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
